4f976d3354530f7d7f96dff9335ef78b722a1f4f
Дарья Серова <dashaflay2000@gmail.com>
2025-12-19 22:00:18 +0300

    System tray icon DONE

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -19,6 +19,16 @@ namespace Clock
 			this.MinimizeBox = false;
 		}
 
+		void SetVisibility(bool visible)
+		{
+			cbShowWeekDay.Visible = visible;
+			cbShowDate.Visible = visible;
+			btnHideControls.Visible = visible;
+			this.ShowInTaskbar = visible;
+			this.FormBorderStyle = visible ? FormBorderStyle.FixedSingle : FormBorderStyle.None;
+			this.TransparencyKey = visible ? Color.Empty : this.BackColor;
+		}
+
 		private void timer_Tick(object sender, EventArgs e)
 		{
 			labelTime.Text=DateTime.Now.ToString
@@ -35,14 +45,37 @@ namespace Clock
 
 			if (cbShowWeekDay.Checked) labelTime.Text+= $"\n{DateTime.Now.DayOfWeek}";
 
+			notifyIcon.Text = labelTime.Text;
 		}
 
 		private void btnHideControls_Click(object sender, EventArgs e)
 		{
-			cbShowWeekDay.Visible = false;
-			cbShowDate.Visible = false;
-			btnHideControls.Visible = false;
-			this.ShowInTaskbar = false;
+			//cbShowWeekDay.Visible = false;
+			//cbShowDate.Visible = false;
+			//btnHideControls.Visible = false;
+			//this.ShowInTaskbar = false;
+			//this.FormBorderStyle = FormBorderStyle.None;
+			//this.TransparencyKey = this.BackColor;
+
+			SetVisibility(false);
+		}
+
+		private void labelTime_MouseHover(object sender, EventArgs e)
+		{
+			//cbShowWeekDay.Visible = true;
+			//cbShowDate.Visible = true;
+			//btnHideControls.Visible = true;
+			//this.ShowInTaskbar = true;
+			//this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			//this.TransparencyKey = Color.Empty;
+
+			SetVisibility(true);
+		}
+
+		private void notifyIcon_DoubleClick(object sender, EventArgs e)
+		{
+			this.TopMost = true;
+			this.TopMost = false;
 		}
 	}
 }

62a5f1ced9fc08c915d6a646ca1583f4cbf5abcb
Дарья Серова <dashaflay2000@gmail.com>
2025-12-19 20:35:11 +0300

    Add project files.

---
+++ Clock/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Clock
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.labelTime = new System.Windows.Forms.Label();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.cbShowDate = new System.Windows.Forms.CheckBox();
+			this.cbShowWeekDay = new System.Windows.Forms.CheckBox();
+			this.btnHideControls = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// labelTime
+			// 
+			this.labelTime.AutoSize = true;
+			this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.labelTime.Location = new System.Drawing.Point(12, 9);
+			this.labelTime.Name = "labelTime";
+			this.labelTime.Size = new System.Drawing.Size(235, 42);
+			this.labelTime.TabIndex = 0;
+			this.labelTime.Text = "CurrentTime";
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// cbShowDate
+			// 
+			this.cbShowDate.AutoSize = true;
+			this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.cbShowDate.Location = new System.Drawing.Point(12, 171);
+			this.cbShowDate.Name = "cbShowDate";
+			this.cbShowDate.Size = new System.Drawing.Size(232, 36);
+			this.cbShowDate.TabIndex = 1;
+			this.cbShowDate.Text = "Показать Дату";
+			this.cbShowDate.UseVisualStyleBackColor = true;
+			// 
+			// cbShowWeekDay
+			// 
+			this.cbShowWeekDay.AutoSize = true;
+			this.cbShowWeekDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.cbShowWeekDay.Location = new System.Drawing.Point(12, 201);
+			this.cbShowWeekDay.Name = "cbShowWeekDay";
+			this.cbShowWeekDay.Size = new System.Drawing.Size(333, 36);
+			this.cbShowWeekDay.TabIndex = 2;
+			this.cbShowWeekDay.Text = "Показать день недели";
+			this.cbShowWeekDay.UseVisualStyleBackColor = true;
+			// 
+			// btnHideControls
+			// 
+			this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btnHideControls.Location = new System.Drawing.Point(19, 243);
+			this.btnHideControls.Name = "btnHideControls";
+			this.btnHideControls.Size = new System.Drawing.Size(314, 77);
+			this.btnHideControls.TabIndex = 3;
+			this.btnHideControls.Text = "Hide Controls";
+			this.btnHideControls.UseVisualStyleBackColor = true;
+			this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(355, 450);
+			this.Controls.Add(this.btnHideControls);
+			this.Controls.Add(this.cbShowWeekDay);
+			this.Controls.Add(this.cbShowDate);
+			this.Controls.Add(this.labelTime);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Clock PV_521";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelTime;
+		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.CheckBox cbShowDate;
+		private System.Windows.Forms.CheckBox cbShowWeekDay;
+		private System.Windows.Forms.Button btnHideControls;
+	}
+}
+
---
+++ Clock/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			labelTime.Text=DateTime.Now.ToString
+				(
+				"hh:mm:ss tt",
+				System.Globalization.CultureInfo.InvariantCulture
+				);
+
+			if (cbShowDate.Checked)
+			{
+				labelTime.Text+= "\n";
+				labelTime.Text+=DateTime.Now.ToString("yyyy.MM.dd");
+			}
+
+			if (cbShowWeekDay.Checked) labelTime.Text+= $"\n{DateTime.Now.DayOfWeek}";
+
+		}
+
+		private void btnHideControls_Click(object sender, EventArgs e)
+		{
+			cbShowWeekDay.Visible = false;
+			cbShowDate.Visible = false;
+			btnHideControls.Visible = false;
+			this.ShowInTaskbar = false;
+		}
+	}
+}
